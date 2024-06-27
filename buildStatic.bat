e402b82da13366a7a5772a737500f88f95fb3ee7
Ninjapenguin16 <rylanmatthewdean@gmail.com>
2024-06-26 20:33:14 -0500

    Edit build script

--- buildStatic.bat
+++ buildStatic.bat
@@ -1,5 +1,5 @@
 windres -i .\src\resource.rc -o .\build\resource.o
 gcc -DINTERCEPTION_STATIC -Wall -c .\src\PressF3Interception.c -o .\build\PressF3Interception.o
-gcc .\build\PressF3Interception.o .\build\resource.o -o .\build\PressF3Interception.exe -l:interception.a -luser32 -lgdi32 -lshell32 -lpsapi -mwindows
+gcc -static .\build\PressF3Interception.o .\build\resource.o -o .\build\PressF3Interception.exe -l:interception.a -luser32 -lgdi32 -lshell32 -lpsapi -mwindows
 
 pause
